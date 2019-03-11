32b7bf9e9b7c19537acccdb431725f1b6dbcd53f
sneaksnotify <48430667+sneaksnotify@users.noreply.github.com>
2019-03-11 15:34:53 +0800

    Create sneaks.user.js

---
+++ sneaks.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Sneaks ATC
+// @namespace    https://
+// @version      1.01
+// @description  Autorun on applicable pages
+// @author       Sneaks
+// @match        *.sotostore.com/en/?sn=*
+// @match        *.nakedcph.com/cart/?sn=*
+// @match        *.sneakersnstuff.com/en/?sn=*
+// @grant        none
+// ==/UserScript==
+
+var _0x3cee=['Missing\x20SN\x20data','log','test','location','href','split','sn=','png.state=','host','www.nakedcph.com','https://www.nakedcph.com/cart/view','https://www.sneakersnstuff.com/en/cart/view','www.sotostore.com','https://www.sotostore.com/en/cart/view'];(function(_0x5283e6,_0x33327f){var _0x229396=function(_0x29e5ff){while(--_0x29e5ff){_0x5283e6['push'](_0x5283e6['shift']());}};_0x229396(++_0x33327f);}(_0x3cee,0x1a5));var _0xc1b4=function(_0x2d9c24,_0xfde792){_0x2d9c24=_0x2d9c24-0x0;var _0x1d2ddf=_0x3cee[_0x2d9c24];return _0x1d2ddf;};(function(){'use strict';console[_0xc1b4('0x0')](_0xc1b4('0x1'));if(window[_0xc1b4('0x2')][_0xc1b4('0x3')][_0xc1b4('0x4')](_0xc1b4('0x5'))[0x1]!==undefined){document['cookie']=_0xc1b4('0x6')+window[_0xc1b4('0x2')]['href'][_0xc1b4('0x4')](_0xc1b4('0x5'))[0x1];switch(window[_0xc1b4('0x2')][_0xc1b4('0x7')]){case _0xc1b4('0x8'):window['location']=_0xc1b4('0x9');break;case'www.sneakersnstuff.com':window[_0xc1b4('0x2')]=_0xc1b4('0xa');break;case _0xc1b4('0xb'):window['location']=_0xc1b4('0xc');break;}}else{console[_0xc1b4('0x0')](_0xc1b4('0xd'));}}());
